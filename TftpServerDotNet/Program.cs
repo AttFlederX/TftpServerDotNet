84b30f8140f2d53d6d3d9afd54bef4608697b1d7
Dmitry Afanasiev <dmitry.afanasiev@cigen.me>
2019-09-30 10:59:06 +0300

    Add project files.

---
+++ TftpServerDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TftpServerDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
